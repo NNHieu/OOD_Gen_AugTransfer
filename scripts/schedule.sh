2f6c1bfb2eca9778ea6c221c7cf631b8957ea1a6
NNHieu <ngochieutb13@gmail.com>
2022-11-10 10:20:18 +0700

    add cifar10_resnet32 experiment config

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -7,3 +7,5 @@ python src/train.py trainer.max_epochs=5 logger=csv
 python src/train.py trainer.max_epochs=10 logger=csv
 
 ckpt_path=$(python -c "from src.utils import rglob_checkpoints; print(rglob_checkpoints('logs/train/runs/mnist/'))")
+
+python src/train.py -m experiment=cifar10 'datamodule/transforms=glob(*)' test=false trainer.max_epochs=3 seed=100

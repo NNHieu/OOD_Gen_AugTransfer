aebd0612c5635bf1cea1dc11d6ef9ec23d9c43d9
NNHieu <ngochieutb13@gmail.com>
2022-11-16 15:23:10 +0700

    fixed eval code follow new aug config

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -9,3 +9,4 @@ python src/train.py trainer.max_epochs=10 logger=csv
 ckpt_path=$(python -c "from src.utils import rglob_checkpoints; print(rglob_checkpoints('logs/train/runs/mnist/'))")
 
 python src/train.py -m experiment=cifar10 'datamodule/transforms=glob(*)' test=false trainer.max_epochs=3 seed=100
+python src/eval.py -m ckpt_path=$ckpt_path datamodule=small_mnist 'datamodule/transforms=glob(*)' seed=101,102,103,104 extras.print_config=False datamodule.batch_size=128
